9ab1cb752f2bbc597d72fde91fd11ce5e3c996a8
rbyers87 <35844687+rbyers87@users.noreply.github.com>
2025-01-10 00:31:34 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ self.addEventListener('install', (event) => {
       return cache.addAll([
         '/',
         '/index.html',
-        '/styles.css',
+        '/index.css',
         '/src/main.tsx',
         '/icons/android-icon-192x192.png',
       ]);
